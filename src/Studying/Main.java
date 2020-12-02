cdb73faa490d1fb489bb5807b0556a05ecee2799
belovdg <belovdg87@yandex.ru>
2020-12-02 11:27:58 +0300

    Переименование пакета

--- src/Studying/Main.java
+++ src/Studying/Main.java
@@ -1,11 +1,10 @@
-package com.company;
+package Studying;
 
 public class Main {
 
     public static void main(String[] args) {
 	int price = 43;
 	int volume = 50;
-
 	System.out.println(price*volume);
     }
 }
